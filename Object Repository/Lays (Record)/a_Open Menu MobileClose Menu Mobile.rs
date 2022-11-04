307334b586d3daa61f97a7aaa43f8588872fa40d
aboal <aboal@DESKTOP-AGCU84K>
2022-11-04 21:34:31 +0300

    New Lays

---
+++ Object Repository/Lays (Record)/a_Open Menu MobileClose Menu Mobile.rs
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Open Menu MobileClose Menu Mobile</name>
+   <tag></tag>
+   <elementGuidId>b16a83d7-1575-4cb3-b349-37ec27cde6f5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>a.to-open-close-menu-mobile</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Search'])[4]/following::a[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>436b7e46-d6f5-4116-ba73-380444aef2bb</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>#</value>
+      <webElementGuid>bd42c439-ae00-40ac-b9ed-6fc05dab6d8d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>to-open-close-menu-mobile</value>
+      <webElementGuid>3a3e752d-1ffe-440f-943f-93f2e5ede150</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+
+Open Menu Mobile 
+Close Menu Mobile
+</value>
+      <webElementGuid>0c4286ad-4f4f-4f24-96e4-2970bae3e35b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;page-home page-node-type-page has-hover menu-hover&quot;]/header[@class=&quot;top-header-container en&quot;]/div[@class=&quot;menu&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;menu-internal&quot;]/nav[1]/a[@class=&quot;to-open-close-menu-mobile&quot;]</value>
+      <webElementGuid>5266bdec-33e6-4ac0-bf5f-b08c06b33bb7</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Search'])[4]/following::a[1]</value>
+      <webElementGuid>b9cbf5a9-dbda-4bf8-b7c5-c133a35e847a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Search'])[3]/following::a[2]</value>
+      <webElementGuid>f69749d5-6004-4192-a47d-d1d1b3788537</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>(//a[contains(@href, '#')])[5]</value>
+      <webElementGuid>48257ea1-1f32-4fe5-9f75-fb6c8de18262</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a[2]</value>
+      <webElementGuid>ccee6060-f354-4080-a2dc-922f5866f52c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '#' and (text() = '
+
+Open Menu Mobile 
+Close Menu Mobile
+' or . = '
+
+Open Menu Mobile 
+Close Menu Mobile
+')]</value>
+      <webElementGuid>8de8b133-e0dc-4efb-bbbd-15dbe93e210f</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
