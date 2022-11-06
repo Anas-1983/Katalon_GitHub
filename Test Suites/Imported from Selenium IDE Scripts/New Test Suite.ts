0d185465f43f2894c4cb9a26b7594c8ef988ec77
aboal <aboal@DESKTOP-AGCU84K>
2022-11-06 11:28:00 +0300

    now

---
+++ Test Suites/Imported from Selenium IDE Scripts/New Test Suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>0</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8ddb50c3-eff4-4327-94bc-bf735f62501f</testSuiteGuid>
+</TestSuiteEntity>
