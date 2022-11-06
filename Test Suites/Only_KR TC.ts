6c45e192bdae09a1ebef09f8fae4e2b9c55059af
aboal <aboal@DESKTOP-AGCU84K>
2022-11-06 16:17:28 +0300

    new 111

---
+++ Test Suites/Only_KR TC.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Only_KR TC</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c71a47b5-6b33-4547-956a-6e80a62777d6</testSuiteGuid>
+   <testCaseLink>
+      <guid>7d6c0408-c4f3-4b77-aecf-6f36c1c87789</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/KR_TCs transfer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
